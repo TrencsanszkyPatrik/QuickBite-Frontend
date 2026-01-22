01d10dc40c9cbab8f3d97b26429a67fa44a70b1b
TrencsanszkyPatrik <trencsanszkyp@kkszki.hu>
2026-01-22 08:35:33 +0100

    Navbar dropdown menu

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,30 +1,63 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import logo from '../../img/LOGO/logo.jpg'
 import { Link } from 'react-router-dom'
 import '../components/css/navbar.css'
 
 export default function Navbar() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+  const dropdownRef = useRef(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
   return (
-    
+
     <header>
-        <div class="header-content">
-            <Link to={"/"}>
-            <div class="logo">
-                <img class="logo-img" src={logo} alt="QuickBite logó" />
-                <span>QuickBite</span>
-            </div>
-            </Link>
+      <div class="header-content">
+        <Link to={"/"}>
+          <div class="logo">
+            <img class="logo-img" src={logo} alt="QuickBite logó" />
+            <span>QuickBite</span>
+          </div>
+        </Link>
+
+        <div class="header-actions">
+          <Link to="/ettermek">Éttermeink</Link>
+          <Link to="/kedvencek">Kedvencek</Link>
+
+          <div class="dropdown" ref={dropdownRef}>
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            >
+              <i class="bi bi-list"></i>Menü
+            </button>
+            {isDropdownOpen && (
+              <div class="dropdown-menu">
+
+                <Link to="/velemenyek">Vásárlók véleményei</Link>
+
+                <Link to="/kapcsolat">Kapcsolat</Link>
+                <Link to="/rolunk">Rólunk</Link>
+              </div>
+            )}
+          </div>
+
             
-            <div class="header-actions">
-                <Link to = "/kapcsolat">Kapcsolat</Link>
-                <Link to = "/rolunk">Rólunk</Link>
-                <Link to ="/ettermek">Éttermeink</Link>
-                <Link to ="/kedvencek">Kedvencek</Link>
-                <Link to ="/velemenyek">Vásárlók véleményei</Link>
-                <Link to ="/kosar"><button class="btn btn-secondary"><i class="bi bi-basket2-fill"></i>Kosár</button></Link>
-                <Link to ="/bejelentkezes"><button class="btn btn-primary"><i class="bi bi-person-circle"></i>Bejelentkezés</button></Link>
-            </div>
+          <Link to="/kosar"><button class="btn btn-secondary"><i class="bi bi-basket2-fill"></i>Kosár</button></Link>
+          <Link to="/bejelentkezes"><button class="btn btn-primary"><i class="bi bi-person-circle"></i>Bejelentkezés</button></Link>
         </div>
+      </div>
     </header>
 
   )
